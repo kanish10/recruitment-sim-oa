3e0fbdb7cd426db083be52c0a9befc0c4db82449
Kanish Khanna <kkhanna7@student.ubc.ca>
2024-09-12 22:32:48 -0700

    Function written!

--- disk.py
+++ disk.py
@@ -13,3 +13,35 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+
+    if (height < 0):
+        raise Exception("Height must not be negative.")
+
+    g = 9.81
+    final_velocity_bottom = np.sqrt(((4/3) * g * height))
+    return final_velocity_bottom
+
+# test function
+def test_func_1():
+    assert final_disk_speed(0, 0, 0, 0, 0, 0) == 0.0
+def test_func_2():
+    assert np.isclose(final_disk_speed(1, 1, 1, 1, 1, 1), 3.616628264, atol=1e-6)
+
+# test exception
+def test_exception():
+    try:
+        final_disk_speed(-1, 0, 0, 0, 0, 0)
+    except Exception as e:
+        assert str(e) == "Height must not be negative."
+        print("Test passed: Exception correctly raised.")
+    else:
+        assert False, "Test failed: Exception was not raised."
+
+def tests():
+    test_func_1()
+    test_func_2()
+    test_exception()
+    print("All tests passed!")
+
+if __name__ == "__main__":
+    tests()
